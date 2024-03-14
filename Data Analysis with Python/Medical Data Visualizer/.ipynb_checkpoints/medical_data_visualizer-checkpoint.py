b9f274c2f0264a9a5fc3d1c8673da1a57b0811a7
martinjendryka <martinjendryka@googlemail.com>
2024-03-14 22:47:21 +0100

    update

--- Data Analysis with Python/Medical Data Visualizer/.ipynb_checkpoints/medical_data_visualizer-checkpoint.py
+++ Data Analysis with Python/Medical Data Visualizer/.ipynb_checkpoints/medical_data_visualizer-checkpoint.py
@@ -20,33 +20,31 @@ df.loc[df['cholesterol']>0,'cholesterol'] = 1
 df.loc[df['gluc']>0,'gluc'] = 1
 
 # Draw Categorical Plot
-def draw_cat_plot():
+def draw_cat_plot(df):
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df,id_vars = ['cardio'],value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     df_cat = df_cat.groupby(['cardio', 'variable'])['value'].value_counts()
     df_cat = df_cat.reset_index()
-    df_cat.rename(columns={'count': 'total'}, inplace=True)
-    # Draw the catplot with 'sns.catplot()'
-    sns.catplot(data= df_cat, x='variable', y = 'total',hue='value',col='cardio',kind='bar')
 
-    fig = plt.gcf()
+    # Draw the catplot with 'sns.catplot()'
+    fig= sns.catplot(data= df_cat, x='variable', y = 'count',hue='value',col='cardio',kind='bar')
+
     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
 # Draw Heat Map
-def draw_heat_map():
+def draw_heat_map(df):
     # Clean the data
     df_heat = df.copy()
-    df_heat =df_heat.loc[(df_heat['ap_lo'] <= df_heat['ap_hi']),:]
-    df_heat =df_heat.loc[(df_heat['height'] >= df_heat['height'].quantile(0.025)),:]
-    df_heat =df_heat.loc[(df_heat['height'] <= df_heat['height'].quantile(0.975)),:]
-    df_heat =df_heat.loc[(df_heat['weight'] >= df_heat['weight'].quantile(0.025)),:]
-    df_heat =df_heat.loc[(df_heat['weight'] <= df_heat['weight'].quantile(0.975)),:]
-    new_order = ['id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight']
-    df_heat = df_heat.loc[:,new_order]
+    df_heat =df_heat.loc[df_heat['ap_lo']<=df_heat['ap_hi'],:]
+    df_heat =df_heat.loc[df_heat['height'] >= df_heat['height'].quantile(0.025),:]
+    df_heat =df_heat.loc[df_heat['height'] <= df_heat['height'].quantile(0.975),:]
+    df_heat =df_heat.loc[df_heat['weight'] >= df_heat['weight'].quantile(0.025),:]
+    df_heat =df_heat.loc[df_heat['weight'] <= df_heat['weight'].quantile(0.975),:]
+
     # Calculate the correlation matrix
     corr = df_heat.corr()
 
