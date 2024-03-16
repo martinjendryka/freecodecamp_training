21d07619d19058b84bd9d8139c20bad06b763436
martinjendryka <martinjendryka@googlemail.com>
2024-03-16 12:17:37 +0100

    seaLevelPredictor

--- Data Analysis with Python/Page View Time Series Visualizer/time_series_visualizer.py
+++ Data Analysis with Python/Page View Time Series Visualizer/time_series_visualizer.py
@@ -5,18 +5,20 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
-
+file_name = 'fcc-forum-pageviews.csv'
+df = pd.read_csv(file_name)
+df.index = pd.date_range(start="2016-05-09",end = "2019-12-03")
+df= df.drop("date",axis=1)
 # Clean data
-df = None
+df = df.loc[(df['value']>df['value'].quantile(0.025)) & (df['value']<df['value'].quantile(0.975)),:]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig,ax = plt.subplots()
+    df['value'].plot(ax=ax,title="Daily freeCodeCamp Forum Page Views 5/2016-12/2019",color="red")
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,14 +26,20 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.resample('ME').mean()    
+    df_bar['Months'] = df_bar.index.strftime('%B')
+    df_bar['year'] = df_bar.index.year
+    month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    df_bar['Months'] = pd.Categorical(df_bar['Months'], categories=month_order, ordered=True)
 
     # Draw bar plot
+    fig,ax = plt.subplots()
+    sns.barplot(data= df_bar,x='year', y = 'value',hue='Months',ax=ax,legend=True)
+    ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
 
-
-
-
-
+    ax.set_xlabel('Years')
+    ax.set_ylabel('Average Page Views')
+    
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
@@ -39,16 +47,31 @@ def draw_bar_plot():
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
-    df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+    #df_box.reset_index(inplace=True)
+    df_box['month'] = df_box.index.strftime('%B')
+    month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+    df_box['month'] = pd.Categorical(df_box['month'], categories=month_order, ordered=True)
+    df_box['year'] = df_box.index.year
+    
     # Draw box plots (using Seaborn)
+    fig,axs = plt.subplots(nrows=1,ncols=2)
+    sns.boxplot(data= df_box,x='year', y = 'value',ax=axs[0])
+    axs[0].set_title('Year-wise Box Plot (Trend)')
+    axs[0].set_xlabel('Year')
+    axs[0].set_ylabel('Page Views')
 
+    sns.boxplot(data= df_box,x='month', y = 'value',ax=axs[1])
+    axs[1].set_title('Month-wise Box Plot (Seasonality)')
+    axs[1].set_xlabel('Month')
+    axs[1].set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 
-
-
+    axs[1].set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
     return fig
+
+
+
+ 
--- Data Analysis with Python/Sea Level Predictor/sea_level_predictor.py
+++ Data Analysis with Python/Sea Level Predictor/sea_level_predictor.py
@@ -4,20 +4,36 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+    file_name= 'epa-sea-level.csv'
+    df = pd.read_csv(file_name)
 
-
+    x = df.loc[:,'Year']
+    y = df.loc[:,'CSIRO Adjusted Sea Level']
     # Create scatter plot
-
-
+    
+    fig,ax = plt.subplots()
+    ax.scatter(x, y)
+    
     # Create first line of best fit
-
-
+    slope, intercept, r_value, p_value, std_err = linregress(x,y)
+    x_2050 = pd.concat([x,pd.Series(range(x.iloc[-1]+1,2051))],ignore_index=True)
+    ax.plot(x_2050,x_2050*slope+intercept,color='red')
+    
     # Create second line of best fit
+    x_new= df.loc[df['Year']>=2000,'Year']
+    y_new = df.loc[df['Year']>=2000,'CSIRO Adjusted Sea Level']
+    
+    slope, intercept, r_value, p_value, std_err = linregress(x_new,y_new)
+    x_2050 = pd.concat([x_new,pd.Series(range(x_new.iloc[-1]+1,2051))],ignore_index=True)
+
+    ax.plot(x_2050,x_2050*slope+intercept,color='green')
 
 
     # Add labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.set_title('Rise in Sea Level')
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
