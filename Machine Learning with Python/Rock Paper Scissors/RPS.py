d03b64362b83dd5ab2220340b29a8bc7eb0a9399
martinjendryka <martinjendryka@googlemail.com>
2024-03-19 16:46:38 +0100

    rockpaperscissorProject

--- Machine Learning with Python/Rock Paper Scissors/RPS.py
+++ Machine Learning with Python/Rock Paper Scissors/RPS.py
@@ -1,49 +1,100 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[],counter=[0],player_history=[],isquincy=[False]):
+def player(prev_play, opponent_history=[],player_history=[],counter=[0], quincyswitch=[False],
+           play_order=[{
+               "RR": 0,
+               "RP": 0,
+               "RS": 1,
+               "PR": 0,
+               "PP": 0,
+               "PS": 0,
+               "SR": 0,
+               "SP": 0,
+               "SS": 0,
+           }]):
     
     opponent_history.append(prev_play)
     if not prev_play:
         player_history.append(prev_play)
-                          
+        counter[0] = 0
+        quincyswitch[0] = False
+        
+    abbey_prevchoice = "P"
+        
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    thisswitch = False
+    
+    
+    # abbey choice
+    if len(list(filter(lambda x: not x, player_history[-3:]))) == 0:    
+        thisswitch = True
+        last_twoPrevious = "".join(player_history[-3:-1])
+        play_orderPrevious=play_order[0].copy()
+        play_orderPrevious[last_twoPrevious] -= 1
+        potential_plays = [
+            player_history[-2] + "R",
+            player_history[-2] + "P",
+            player_history[-2] + "S",
+            ]
+        sub_order = {
+            k: play_orderPrevious[k]
+            for k in potential_plays if k in play_orderPrevious
+            }
 
-    if opponent_history[-5:] == ["R","R","P","P","S"]: # strategy for Quincy (100% win)
-        isquincy[0] = [True]
+        abbey_prevchoice1 = ideal_response[max(sub_order, key=sub_order.get)[-1]]
+        abbey_prevchoice2 = ideal_response[max(sub_order, key=sub_order.get)[0]]
         
-    if isquincy[0]:
+
+   
+    last_two = "".join(player_history[-2:])
+    if len(last_two) == 2:
+       play_order[0][last_two] += 1
+
+
+    quincy_choices = ["R", "R", "P", "P", "S"]
+
+    if opponent_history[-5:] == quincy_choices: # strategy for Quincy
+       quincyswitch[0] = True
         
-        choices = ["R", "R", "P", "P", "S"]
-        guess = choices[counter[0] % len(choices)]
-        counter[0] += 1
+    if quincyswitch[0]:
+        guess = quincy_choices[counter[0] % len(quincy_choices)]
+        counter[0] +=1      
+
     else:
-        last_ten = player_history[-10:]
-        most_frequent =  max(set(last_ten), key=last_ten.count)
+        guess = "P"
+        if thisswitch:
         
-        if most_frequent == '':
-            most_frequent = "S"
-            guess = ideal_response[most_frequent]
-
-        elif prev_play == ideal_response[most_frequent]: #mrugesh strategy
-            guess = ideal_response[most_frequent]
-       
-        elif len(player_history)>1:
+            last_ten = player_history[-11:-1]
+            most_frequent =  max(set(last_ten), key=last_ten.count)
             
-            if player_history[-1]:
-                if prev_play == ideal_response[player_history[-1]]:  # strategy for kris
-                # kris spielt immer das was in der letzten runde gespielt wurde,
-                    guess =  player_history[-1]
-                else:
-                    guess = "R"
-            else:
-                guess="R"
-                    
-        else:
-            guess = "R"
+            if most_frequent == '':
+                most_frequent = "S"
+                guess = ideal_response[most_frequent]
+
+            elif prev_play == ideal_response[most_frequent]: #mrugesh strategy
+                last_ten = player_history[-10:]
+                most_frequent =  max(set(last_ten), key=last_ten.count)
         
+                guess = ideal_response[most_frequent] 
+            
+            if ''.join(opponent_history[-2:])== abbey_prevchoice1 + abbey_prevchoice2: #abbey strategy
+             
+                potential_plays = [
+                    player_history[-1] + "R",
+                    player_history[-1] + "P",
+                    player_history[-1] + "S",
+                    ]
+                sub_order = {
+                    k: play_order[0][k]
+                    for k in potential_plays if k in play_order[0]
+                    }
+
+                guess = ideal_response[max(sub_order, key=sub_order.get)[-1:]]
+             
+            elif player_history[-2]: # kris strategy
+                if prev_play == ideal_response[player_history[-2]]:  # strategy for kris
+             # kris spielt immer das was in der letzten runde gespielt wurde,
+                 guess =  ideal_response[player_history[-1]]                
+                    
     player_history.append(ideal_response[guess])
-    #print('player',ideal_response[guess])
     return ideal_response[guess]
-
-
-
--- Machine Learning with Python/Rock Paper Scissors/main.py
+++ Machine Learning with Python/Rock Paper Scissors/main.py
@@ -4,10 +4,10 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-play(player, quincy, 1000)
-#play(player, abbey, 1000)
-#play(player, kris, 1000)
-play(player, mrugesh, 1000)
+# play(player, abbey, 1000,True)
+# play(player, kris, 1000)
+# play(player, mrugesh, 1000)
+# play(player, quincy, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -19,3 +19,4 @@ play(player, mrugesh, 1000)
 
 # Uncomment line below to run unit tests automatically
 main(module='test_module', exit=False)
+
