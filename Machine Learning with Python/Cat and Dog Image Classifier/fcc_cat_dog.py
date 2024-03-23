21ef5f73acb1a526a3e854f46013249f90540ff6
martinjendryka <martinjendryka@googlemail.com>
2024-03-23 23:48:57 +0100

    catDocImageClassifier

---
+++ Machine Learning with Python/Cat and Dog Image Classifier/fcc_cat_dog.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 16:58:07 2024
+
+@author: martinjendryka
+"""
+
+# try:
+#   # This command only in Colab.
+#   %tensorflow_version 2.x
+# except Exception:
+#   pass
+
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+# Get number of files in each directory. The train and validation directories
+# each have the subdirecories "dogs" and "cats".
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = len(os.listdir(test_dir))
+
+# Variables for pre-processing and training.
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+
+
+# 3
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+test_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(train_dir,
+                                                           target_size=(IMG_WIDTH,IMG_HEIGHT),
+                                                           batch_size=batch_size,class_mode = 'binary')
+
+val_data_gen =  validation_image_generator.flow_from_directory(validation_dir,
+                                                           target_size=(IMG_WIDTH,IMG_HEIGHT),
+                                                           batch_size=batch_size,class_mode = 'binary')
+
+test_data_gen =  test_image_generator.flow_from_directory(directory=PATH,
+                                                           target_size=(IMG_WIDTH,IMG_HEIGHT),classes=['test'],
+                                                           class_mode=None,shuffle=False,batch_size=batch_size)
+
+# # 4 plottet die bilder als subplot mit 5 rows
+def plotImages(images_arr, probabilities = False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    plt.show()
+
+# sample_training_images, _ = next(train_data_gen) # get images of batch
+# plotImages(sample_training_images[:5])
+
+
+
+# # 5 # apply random transformations to image training data
+train_image_generator = ImageDataGenerator(rescale=1./255, 
+                                           shear_range=0.2,
+                                           width_shift_range = 0.2,
+                                           height_shift_range=0.2,
+                                           zoom_range=0.2,
+                                           rotation_range=360,
+                                          horizontal_flip=True, 
+                                           brightness_range=[0.7, 1.3],
+                                           featurewise_center=True)
+
+# # 6
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                      directory=train_dir,
+                                                      target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                      class_mode='binary')
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+
+# # 7
+model = Sequential()
+
+# add layers to model
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Print the model
+model.summary()
+
+
+# # 8
+history= model.fit(
+    train_data_gen,
+    steps_per_epoch=train_data_gen.samples // batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=val_data_gen.samples // batch_size)
+
+
+# # 9
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# make predictions
+predictions = model.predict(test_data_gen)
+
+sample_test_images = next(test_data_gen) # get images of batch
+
+plotImages(sample_test_images, predictions)
+# # 11
+answers =  [1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0,
+            1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0,
+            1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+            1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1,
+            0, 0, 0, 0, 0, 0]
+
+correct = 0
+probabilities = predictions.flatten().tolist()
+for probability, answer in zip(probabilities, answers):
+   if round(probability) == answer:
+     correct +=1
+
+percentage_identified = (correct / len(answers)) * 100
+
+passed_challenge = percentage_identified >= 63
+
+print(f"Your model correctly identified {round(percentage_identified, 2)}% of the images of cats and dogs.")
+
+if passed_challenge:
+  print("You passed the challenge!")
+else:
+  print("You haven't passed yet. Your model should identify at least 63% of the images. Keep trying. You will get it!")
+  
